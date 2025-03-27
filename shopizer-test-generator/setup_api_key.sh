29afa6a32ce8da9ebaa8db2822d98e72fa713975
ibrakel <113463426+ibrakel@users.noreply.github.com>
2025-03-27 16:13:21 +0100

    hide key

--- shopizer-test-generator/setup_api_key.sh
+++ shopizer-test-generator/setup_api_key.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Set your OpenAI API key here
-export OPENAI_API_KEY="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"
-
+export OPENAI_API_KEY=""
 # Verify the key is set
 echo "OpenAI API key has been set as an environment variable."
 echo "You can now run any of the test generator scripts."
